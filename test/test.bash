a5fc0653a0fb717227a31c966c9c5731561c6da9
ftomo <zhiguitengyuan@gmail.com>
2025-12-26 20:55:47 +0900

    test.bash修正

--- test/test.bash
+++ test/test.bash
@@ -10,12 +10,13 @@ cd $dir/ros2_ws
 colcon build
 
 # 2. 環境設定の読み込み
+source /opt/ros/humble/setup.bash
 # GitHub Actions等では .bashrc ではなく install/setup.bash を直接読み込むのが確実です
 source install/setup.bash
 
 # 3. ノードの実行（15秒間実行してログを保存）
 # ここでは mypkg2 の talker ノード（電圧出力）を実行すると仮定します
-timeout 15 ros2 run mypkg2 talker > /tmp/mypkg.log
+timeout 15 ros2 run mypkg2 shunt > /tmp/mypkg.log
 
 # 4. 判定
 # ログの中に「送信中の電圧:」という文字列があるか、
